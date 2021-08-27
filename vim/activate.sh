cf4758a62593b00ead516e8bbb2e6d681d0dd99e
vagrant <vagrant@localhost.localdomain>
2021-08-27 22:51:15 +0000

    update: modified vimrc installer to be a safe program

--- vim/activate.sh
+++ vim/activate.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -eu 
 
-cp ./.vimrc ~/
-cp ./.gvimrc ~/
+src_files_array=(".vimrc" ".gvimrc")
+target_base_path=~/
 
-cd ~
+for file in ${src_files_array[@]}; do
+    
+    if [[ ! -f "${target_base_path}${file}" ]]; then
+        echo "Exec: cp ${file} ${target_base_path}${file}"
+        cp ${file} ${target_base_path}${file}
+        echo "${target_base_path}${file} created successfully"
+        ls -l ${target_base_path}${file}
+                
+    else
+        echo "${target_base_path}${file} already exists..."
+    fi
+
+done
+
+echo "Completed!!!"
